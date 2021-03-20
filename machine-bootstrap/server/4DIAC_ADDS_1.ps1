8bec2c3ad027a6ce2a59917aa9e57e9e972a139d
Brad <bbk.brad@gmail.com>
2021-03-20 09:27:00 -0500

    adds_1 update

--- machine-bootstrap/server/4DIAC_ADDS_1.ps1
+++ machine-bootstrap/server/4DIAC_ADDS_1.ps1
@@ -4,13 +4,7 @@ $hostname = Read-Host
 (Get-WmiObject Win32_ComputerSystem).Rename($hostname)
 
 ### Set Static IP
-Write-Host -ForegroundColor Yellow 'Please enter IP address, Gateway, and Subnet CIDR' `n
-$ipaddress = Read-Host
-Write-Host -ForegroundColor Yellow 'Please enter a hostname for the domain controller.' `n
-$gateway = Read-Host
-Write-Host -ForegroundColor Yellow 'Please enter a hostname for the domain controller.' `n
-$subnetmask = Read-Host
-New-NetIPAddress –IPAddress $ipaddress -DefaultGateway $gateway -PrefixLength $subnetmask -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+New-NetIPAddress –IPAddress 172.16.0.11 -DefaultGateway 172.16.0.1 -PrefixLength 16 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
 
 ### Reboot
 Invoke-Reboot
