36727b41c4e8a879b8f4b0d845d9e3c504d30dc4
Brad <bbk.brad@gmail.com>
2021-03-20 09:32:25 -0500

    changes

--- machine-bootstrap/server/4DIAC_ADDS_1.ps1
+++ machine-bootstrap/server/4DIAC_ADDS_1.ps1
@@ -4,7 +4,7 @@ $hostname = Read-Host
 (Get-WmiObject Win32_ComputerSystem).Rename($hostname)
 
 ### Set Static IP
-New-NetIPAddress –IPAddress 172.16.0.11 -DefaultGateway 172.16.0.1 -PrefixLength 16 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
+New-NetIPAddress -IPAddress 172.16.0.11 -DefaultGateway 172.16.0.1 -PrefixLength 16 -InterfaceIndex (Get-NetAdapter).InterfaceIndex
 
 ### Reboot
 Invoke-Reboot
