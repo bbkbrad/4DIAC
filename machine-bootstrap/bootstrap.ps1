6b1d6d9ca770c849ff56405bf564cd1669f322f5
Brad <bbk.brad@gmail.com>
2021-03-20 12:11:26 -0500

    changed encoding

--- machine-bootstrap/bootstrap.ps1
+++ machine-bootstrap/bootstrap.ps1
@@ -1,4 +1,4 @@
-####  install chocolatey
+﻿####  install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 
