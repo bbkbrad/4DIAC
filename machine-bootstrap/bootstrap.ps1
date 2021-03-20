7738f52502393b35807c41c3978b4fede715b54e
Brad <bbk.brad@gmail.com>
2021-03-20 09:16:06 -0500

    updated adds configuration

--- machine-bootstrap/bootstrap.ps1
+++ machine-bootstrap/bootstrap.ps1
@@ -14,32 +14,33 @@ Do {
     Write-Host -ForegroundColor Green '   3. Base Server'
     Write-Host -ForegroundColor Green '   4. Domain Controller'
     $selection = Read-Host
+
     If ($selection -eq "1")
         {
             Write-Host -ForegroundColor Yellow 'You selected:'
             Write-Host -ForegroundColor Green -NoNewLine '  Base Workstation' `n
-            Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/workstation/bs-workstation.ps1?token=AN6IDO3VGGQS6FCFH5LKZBLAKVWII
+            Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/workstation/bs-workstation.ps1 
             $success = 1
         }
     Elseif ($selection -eq "2")
         {
             Write-Host -ForegroundColor Yellow 'You selected:'
             Write-Host -ForegroundColor Green -NoNewLine '  Brads Workstation' `n
-            Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/workstation/bs-brad.ps1?token=AN6IDO6VMA2JO7VQ27VLU2TAKVWJ2
+            Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/workstation/bs-brad.ps1
             $success = 1
         }
     Elseif ($selection -eq "3")
         {
             Write-Host -ForegroundColor Yellow 'You selected:'
             Write-Host -ForegroundColor Green -NoNewLine '  Base Server' `n
-            Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/bs-server.ps1?token=AN6IDO53XJ766P7E6OIXY3TAKVWLM
+            Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/bs-server.ps1
             $success = 1
         }
     Elseif ($selection -eq "4")
         {
             Write-Host -ForegroundColor Yellow 'You selected:'
             Write-Host -ForegroundColor Green -NoNewLine '  Domain Controller' `n
-            Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/bs-adds.ps1?token=AN6IDO2JXBSI4HBCPEFDCM3AKVWM6
+            Install-BoxstarterPackage -PackageName https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/bs-adds.ps1
             $success = 1
         }
     Else{
--- machine-bootstrap/server/bs-adds.ps1
+++ machine-bootstrap/server/bs-adds.ps1
@@ -1,14 +1,14 @@
 ### get credentials for reboots
-$cred=Get-Credential domain\username
+$cred = Get-Credential administrator
 
 ### update OS
 Enable-MicrosoftUpdate
 Install-WindowsUpdate -AcceptEula
 
 ### boxstarter package url
-$4DIAC_ADDS_1 = 'https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/4DIAC_ADDS_1.ps1?token=AN6IDO5TZ2WSCATZ2C3NLPDAKVXN2'
-$4DIAC_ADDS_2 = 'https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/4DIAC_ADDS_2.ps1?token=AN6IDO2QVPWMSDWIJXCWARDAKVXPY'
-$4DIAC_ADDS_3 = 'https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/4DIAC_ADDS_3.ps1?token=AN6IDO2HJ77SDV22F7TGGI3AKVXQS'
+$4DIAC_ADDS_1 = 'https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/4DIAC_ADDS_1.ps1'
+$4DIAC_ADDS_2 = 'https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/4DIAC_ADDS_2.ps1'
+$4DIAC_ADDS_3 = 'https://raw.githubusercontent.com/bbkbrad/4DIAC/main/machine-bootstrap/server/4DIAC_ADDS_3.ps1'
 
 ### install boxstarter packages
 Install-BoxstarterPackage -PackageName $4DIAC_ADDS_1,$4DIAC_ADDS_2,$4DIAC_ADDS_3 -Credential $cred
