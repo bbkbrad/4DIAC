34c74e6c236984d8a1d6b13f3ff19ce1d6d6c680
Brad <bbk.brad@gmail.com>
2021-03-20 12:13:22 -0500

    adjusted order in bootstrap

--- machine-bootstrap/bootstrap.ps1
+++ machine-bootstrap/bootstrap.ps1
@@ -1,12 +1,13 @@
-﻿####  install chocolatey
+﻿### get credentials for reboots
+$cred = Get-Credential $env:COMPUTERNAME'\administrator' 
+
+####  install chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 
 ####  install boxstarter
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://boxstarter.org/bootstrapper.ps1')); Get-Boxstarter -Force
 
-### get credentials for reboots
-$cred = Get-Credential $env:COMPUTERNAME'\administrator' 
 
 ### script selection
 Do {
